b5d48a63c286baf5c05f3e0448409671d3535573
Dupszer <Loann.duplessis@etu.univ-tours.fr>
2021-05-12 15:09:42 +0200

    okok

--- pwojeycepepey/CSommet.cpp
+++ pwojeycepepey/CSommet.cpp
@@ -3,7 +3,9 @@
 
 Csommet::Csommet()
 {
-
+	this->uiSOMNumSom=0;
+	this->SOMPartant=nullptr;
+	this->SOMArrivant=nullptr;
 }
 Csommet::~Csommet()
 {
@@ -11,7 +13,9 @@ Csommet::~Csommet()
 }
 Csommet::Csommet(unsigned int uiArg)
 {
-
+	this->uiSOMNumSom = uiArg;
+	this->SOMPartant = nullptr;
+	this->SOMArrivant = nullptr;
 }
 void Csommet::link(Csommet sommet)
 {
@@ -30,3 +34,28 @@ void Csommet::SOMArcPartant(Carc * ARCArg)
 
 }
 
+int Csommet::tailleArrivant()
+{
+	int i = 0;
+	while (this->SOMArrivant != nullptr)
+	{
+		i++;
+		this->SOMArrivant++;
+	}
+	return i;
+}
+
+int Csommet::taillePartant()
+{
+
+}
+
+void Csommet::suppArcArrivant(Carc * ARCArg)
+{
+
+}
+
+void Csommet::suppArcPartant(Carc* ARCArg)
+{
+
+}
