780109eca4163544a58801f93f761d5673f51822
Dupszer <Loann.duplessis@etu.univ-tours.fr>
2021-05-12 09:33:41 +0200

    ouioui

--- pwojeycepepey/CSommet.h
+++ pwojeycepepey/CSommet.h
@@ -1,11 +1,17 @@
 #pragma once
-class CSommet
+#include "Carc.h"
+class Csommet
 {
-	zfazf
-		azf
-		azf
-		az
-		fazf
-
+private:
+	unsigned int uiSOMNumSom;
+	Carc ** SOMArrivant;
+	Carc ** SOMPartant;
+public:
+	Csommet();
+	Csommet(unsigned int uiArg,Carc ** Lis1,Carc ** Lis2);
+	~Csommet();
+	void SOMModifierNum(unsigned int uiArg);
+	void SOMArcArrivant(Carc** ARCArg);
+	void SOMArcPartant(Carc** ARCArg);
 };
 
