e2afd3cbb97d9f1ff33df3b71b206856ebaae5a7
Paul Poitrineau <paul.poitrineau@etu.univ-tours.fr>
2021-05-29 23:11:06 +0200

    modif

--- pwojeycepepey/pwojeycepepey.cpp
+++ pwojeycepepey/pwojeycepepey.cpp
@@ -7,13 +7,20 @@
 
 int main()
 {
-    Cparser Fichier("test.txt");
+    /*Cparser Fichier("test.txt");
     Cgraphe* graphe1 = new Cgraphe();
 
     Fichier.LireFichier(graphe1);
 
-    graphe1->AfficherGraph();
+    graphe1->AfficherGraph();*/
     
+    Cgraphe graphe1 = Cgraphe();
+    graphe1.createSommet(1);
+    graphe1.createSommet(2);
+    graphe1.createSommet(3);
+    graphe1.createSommet(4);
+
+    graphe1.AfficherGraph();
 
 }
 
